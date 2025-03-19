b48a8f40a9ef2128f397f6eb19e4c64c3c10a267
LeftTwixWand <0965142838gvs@gmail.com>
2025-03-19 18:56:15 +0100

    Remove old namespace

--- src/Inventory.Application/ViewModels/Generic/Messages/GenericItemUpdatedMessage.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.Messaging.Messages;
-
-namespace Inventory.Application.ViewModels.Generic.Messages;
-
-public sealed class GenericItemUpdatedMessage<TModel>(TModel value) : ValueChangedMessage<TModel>(value)
-{
-}
